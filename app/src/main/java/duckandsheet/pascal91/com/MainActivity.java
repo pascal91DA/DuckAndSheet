cac38ef6e0395348cade4056b6cf83c358262917
pascal91 <pascal_91@tut.by>
2019-02-04 01:10:18 +0300

    Попытка в базы данных.

--- app/src/main/java/duckandsheet/pascal91/com/MainActivity.java
+++ app/src/main/java/duckandsheet/pascal91/com/MainActivity.java
@@ -1,6 +1,9 @@
 package duckandsheet.pascal91.com;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -13,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "duckandsheet.pascal91.com.MESSAGE";
 
+    SQLiteDatabase myDB;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,10 +25,41 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        myDB = openOrCreateDatabase("my.db", MODE_PRIVATE, null);
+
+        myDB.execSQL("CREATE TABLE IF NOT EXISTS user (name VARCHAR(200), age INT, is_single INT)");
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                ContentValues row1 = new ContentValues();
+                row1.put("name", "Alice");
+                row1.put("age", 25);
+                row1.put("is_single", 1);
+
+                ContentValues row2 = new ContentValues();
+                row2.put("name", "Bob");
+                row2.put("age", 20);
+                row2.put("is_single", 0);
+
+                myDB.insert("user", null, row1);
+                myDB.insert("user", null, row2);
+
+                Cursor myCursor = myDB.rawQuery("select name, age, is_single from user", null);
+
+                while(myCursor.moveToNext()) {
+                    String name = myCursor.getString(0);
+                    int age = myCursor.getInt(1);
+                    boolean isSingle = (myCursor.getInt(2)) == 1;
+
+                    System.out.println(name + " (" + age + ") - " + isSingle);
+                }
+
+                myCursor.close();
+                myDB.close();
+
                 Intent intent = new Intent(MainActivity.this, NoteViewActivity.class);
                 String message = "Test Стринг";//editText.getText().toString();
                 intent.putExtra(EXTRA_MESSAGE, message);
